410cab380e3367459c1d883f3e4e9e1752965ad2
Manvendra Raj Singh <manvendrarajsinghmanny@gmail.com>
2024-03-06 13:53:01 +0530

    Implementing the preprocessing on dataset

---
+++ tweet_sentiment_analysis.py
@@ -0,0 +1,128 @@
+import numpy as np
+import pandas as pd
+import seaborn as sns
+from wordcloud import WordCloud
+import matplotlib.pyplot as plt
+import requests
+from io import StringIO
+# nltk
+from nltk.stem import WordNetLemmatizer
+
+#Getting the data
+DATASET_COLUMNS=['target','ids','date','flag','user','text']
+DATASET_ENCODING = "ISO-8859-1"
+df = pd.read_csv('data.csv', encoding=DATASET_ENCODING, names=DATASET_COLUMNS)
+
+df.sample(5)
+
+df.head(10)
+
+#Cleaning the data 
+data=df[['text','target']]
+data['target'] = data['target'].replace(4,1)
+data['target'].unique()
+
+data_pos = data[data['target'] == 1]
+data_neg = data[data['target'] == 0]
+
+data_pos = data_pos.iloc[:int(20000)]
+data_neg = data_neg.iloc[:int(20000)]
+
+dataset = pd.concat([data_pos, data_neg])
+
+dataset['text']=dataset['text'].str.lower()
+dataset['text'].tail()
+
+
+#Removing Stowpwords
+stopwordlist = ['a', 'about', 'above', 'after', 'again', 'ain', 'all', 'am', 'an',
+             'and','any','are', 'as', 'at', 'be', 'because', 'been', 'before',
+             'being', 'below', 'between','both', 'by', 'can', 'd', 'did', 'do',
+             'does', 'doing', 'down', 'during', 'each','few', 'for', 'from',
+             'further', 'had', 'has', 'have', 'having', 'he', 'her', 'here',
+             'hers', 'herself', 'him', 'himself', 'his', 'how', 'i', 'if', 'in',
+             'into','is', 'it', 'its', 'itself', 'just', 'll', 'm', 'ma',
+             'me', 'more', 'most','my', 'myself', 'now', 'o', 'of', 'on', 'once',
+             'only', 'or', 'other', 'our', 'ours','ourselves', 'out', 'own', 're','s', 'same', 'she', "shes", 'should', "shouldve",'so', 'some', 'such',
+             't', 'than', 'that', "thatll", 'the', 'their', 'theirs', 'them',
+             'themselves', 'then', 'there', 'these', 'they', 'this', 'those',
+             'through', 'to', 'too','under', 'until', 'up', 've', 'very', 'was',
+             'we', 'were', 'what', 'when', 'where','which','while', 'who', 'whom',
+             'why', 'will', 'with', 'won', 'y', 'you', "youd","youll", "youre",
+             "youve", 'your', 'yours', 'yourself', 'yourselves']
+
+STOPWORDS = set(stopwordlist)
+def cleaning_stopwords(text):
+    return " ".join([word for word in str(text).split() if word not in STOPWORDS])
+dataset['text'] = dataset['text'].apply(lambda text: cleaning_stopwords(text))
+dataset['text'].head()
+
+#removing punctuations
+import string
+english_punctuations = string.punctuation
+punctuations_list = english_punctuations
+def cleaning_punctuations(text):
+    translator = str.maketrans('', '', punctuations_list)
+    return text.translate(translator)
+dataset['text']= dataset['text'].apply(lambda x: cleaning_punctuations(x))
+dataset['text'].tail()
+
+#removing repeating characters
+import re
+def cleaning_repeating_char(text):
+    return re.sub(r'(.)1+', r'1', text)
+dataset['text'] = dataset['text'].apply(lambda x: cleaning_repeating_char(x))
+dataset['text'].tail()
+
+#removing URLs
+def cleaning_URLs(data):
+    return re.sub('((www.[^s]+)|(https?://[^s]+))',' ',data)
+dataset['text'] = dataset['text'].apply(lambda x: cleaning_URLs(x))
+dataset['text'].tail()
+
+#removing numeric characters
+def cleaning_URLs(data):
+    return re.sub('((www.[^s]+)|(https?://[^s]+))',' ',data)
+dataset['text'] = dataset['text'].apply(lambda x: cleaning_URLs(x))
+dataset['text'].tail()
+
+
+#Tokenization
+from nltk.tokenize import RegexpTokenizer
+tokenizer = RegexpTokenizer(r'w+')
+dataset['text'] = dataset['text'].apply(tokenizer.tokenize)
+dataset['text'].head()
+
+#Stemming using Porter-Stemmer Algo
+import nltk
+st = nltk.PorterStemmer()
+def stemming_on_text(data):
+    text = [st.stem(word) for word in data]
+    return data
+dataset['text']= dataset['text'].apply(lambda x: stemming_on_text(x))
+dataset['text'].head()
+
+#Lemmatisation
+lm = nltk.WordNetLemmatizer()
+def lemmatizer_on_text(data):
+    text = [lm.lemmatize(word) for word in data]
+    return data
+dataset['text'] = dataset['text'].apply(lambda x: lemmatizer_on_text(x))
+dataset['text'].head()
+
+
+X=data.text
+y=data.target
+
+#WordCloud visualisation of positive & negative tweets
+data_neg = data['text'][:800000]
+plt.figure(figsize = (20,20))
+wc = WordCloud(max_words = 1000 , width = 1600 , height = 800,
+               collocations=False).generate(" ".join(data_neg))
+plt.imshow(wc)
+
+data_neg = data['text'][:800000]
+plt.figure(figsize = (20,20))
+wc = WordCloud(max_words = 1000 , width = 1600 , height = 800,
+               collocations=False).generate(" ".join(data_neg))
+plt.imshow(wc)
